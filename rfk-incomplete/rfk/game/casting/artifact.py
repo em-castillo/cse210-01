5509dd3908379113cf364432b0f75a6a4b657151
Emily <cas20037@byui.edu>
2022-02-16 18:34:30 -0700

    artifact class comments

--- rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -4,14 +4,23 @@ from game.casting.actor import Actor
 
 
 class Artifact(Actor):
-    "Provides a message when the robot touches an artifact "
+    """Provides a message when the robot touches an artifact 
+    Attributes:
+        Actor class attributes
+        message (message): string message to display"""
 
     def __init__(self):
         super().__init__()
         self.message = ""
 
     def get_message(self):
+        """Gets the artifact's message
+        Returns:
+            string: The artifact's message """
         return self.message
 
     def set_message(self, message):
+        """Updates the message to the given value
+        Args:
+            message (string): The given value."""
         self.message = message
